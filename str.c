a2740c202c25b7a2f0624a21e0e088485680731e
NikkiSmith <nicolealiceowano05@gmail.com>
2025-11-18 20:43:17 +0300

    Initial commit

---
+++ str.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    char array[50];
+    printf("enter a string:");
+    fgets(array, sizeof(array), stdin);
+    printf("you entered: %s\n", array);
+    
+    int counter =0;
+    for(int i = 0; array[i] != '\0'; ++i)
+    {
+        ++counter;
+    }
+    printf("string lemgth: %d", counter);
+
+    return 0;
+
+}
